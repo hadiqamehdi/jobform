dd81c6a750805a251456e25c38677b60a2be8921
Hadiqa Mehdi <110409065+hadiqamehdi@users.noreply.github.com>
2024-01-16 00:12:55 +0500

    Add files via upload

---
+++ Jobform/script.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    // You can add additional validation or form submission logic here
+    alert('Application submitted successfully!');
+    // Reset the form after submission if needed
+    document.getElementById('jobApplicationForm').reset();
+}
